2af9fc3cc0e0171c67cca4d154521168be447099
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-20 15:43:23 -0300

    created Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+
+import { Link } from 'react-router-dom'
 import logoSpotify from '../assets/logo/spotify-logo.png'
 
 const Header = () => {
   return (
-    <div>
-        <img src={logoSpotify} alt="" />
+    <div className="flex border-b-2 border-black gap-3 p-4 bg-black">
+       <Link to="/" className="flex items-center gap-2"> 
+          <img src={logoSpotify} alt="Logo Spotify" className="h-8" />
+          <h1 className="text-amber-50 pt-1 font-bold text-lg">Spotify</h1>
+        </Link>
     </div>
   )
 }
