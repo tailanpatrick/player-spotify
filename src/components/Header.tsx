144c885806d8ad1ebe32ff9c4968ad5d0b6b73e8
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-20 11:15:03 -0300

    intial config and added logo in Header component and favicon

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logoSpotify from '../assets/logo/spotify-logo.png'
+
+const Header = () => {
+  return (
+    <div>
+        <img src={logoSpotify} alt="" />
+    </div>
+  )
+}
+
+export default Header
