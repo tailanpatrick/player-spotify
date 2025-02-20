7180cd3003f84fe73811b7e2a5bbae0deade87b1
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-20 15:58:34 -0300

    ajusted artist name alignment again

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -15,7 +15,7 @@ const Main = () => {
                 <div className="group flex flex-col items-center py-3 px-0 rounded-md bg-transparent hover:bg-[rgba(49,50,50,1)] transition duration-300 ease-in-out cursor-pointer">
                     <div className="relative overflow-hidden flex justify-center items-center">
                         <img
-                            className="rounded-full w-[120px] h-[120px] md:w-[170px] md:h-[170px] overflow-hidden"
+                            className="rounded-full w-[120px] h-[120px] md:w-[155px] md:h-[155px] overflow-hidden"
                             src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
                             alt="Imagem do artista Henrique & Juliano"
                         />
@@ -24,16 +24,15 @@ const Main = () => {
                             className="absolute text-5xl right-2 bottom-2 text-[#1ed760] opacity-0 group-hover:opacity-100 transition-opacity duration-100 ease-in-out z-10"
                         />
                     </div>
-                    <div className="w-full justify-start px-3 py-2">
+                    <div className="w-full justify-start px-5 py-2">
                         <p className="text-md">Henrique & Juliano</p>
                         <p className="text-sm">Artista</p>
                     </div>
                 </div>
-                
                 <div className="group flex flex-col items-center py-3 px-0 rounded-md bg-transparent hover:bg-[rgba(49,50,50,1)] transition duration-300 ease-in-out cursor-pointer">
                     <div className="relative overflow-hidden flex justify-center items-center">
                         <img
-                            className="rounded-full w-[120px] h-[120px] md:w-[170px] md:h-[170px] overflow-hidden"
+                            className="rounded-full w-[120px] h-[120px] md:w-[155px] md:h-[155px] overflow-hidden"
                             src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
                             alt="Imagem do artista Henrique & Juliano"
                         />
@@ -42,12 +41,11 @@ const Main = () => {
                             className="absolute text-5xl right-2 bottom-2 text-[#1ed760] opacity-0 group-hover:opacity-100 transition-opacity duration-100 ease-in-out z-10"
                         />
                     </div>
-                    <div className="w-full justify-start px-3 py-2">
+                    <div className="w-full justify-start px-5 py-2">
                         <p className="text-md">Henrique & Juliano</p>
                         <p className="text-sm">Artista</p>
                     </div>
                 </div>
-
             </div>
         </div>
 
