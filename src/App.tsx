7b14c82da9ef50a2145d30cdf84d61681dfcdf87
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-22 12:06:54 -0300

    creating routes to the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import Header from "./components/Header"
-import Main from "./components/Main"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Artists from "./pages/Artists"
+import Artist from "./pages/Artist"
+import Songs from "./pages/Songs"
+import Song from "./pages/Song"
+
 
 const App = () => {
   return (
     <div className="flex flex-col h-screen">
-      <Header />
-      <Main />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/artists" element={<Artists />}/>
+          <Route path="/songs" element={<Songs />}/>
+          <Route path="/artists/:id" element={<Artist />}/>
+          <Route path="/songs/:id" element={<Song />}/>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   )
 }
