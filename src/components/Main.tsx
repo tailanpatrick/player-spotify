76546c2a646e0b83ac1bc630a80e081cb4db799f
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-22 11:48:35 -0300

    passing props to the ItemList child component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,12 @@
 import ItemList from './ItemList';
+import { artistArray } from './../assets/database/artists.ts';
+import { songsArray } from './../assets/database/songs.ts';
 
 const Main = () => {
     return (
         <div className="flex flex-col flex-1 p-5 mt-2 mx-0 rounded-xl overflow-auto bg-[linear-gradient(180deg,rgba(18,18,18,1)_0%,rgba(83,83,83,0)_100%)]">
-            <ItemList />
-            <ItemList />
+            <ItemList title="Artistas" items={7} itemsArray={artistArray} path="/artists"/>
+            <ItemList title="Músicas" items={14}  itemsArray={songsArray} path="/songs"/>
         </div>
 
     );
