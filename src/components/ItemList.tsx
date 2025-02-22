ac171fbdc9b298a66e56df6df98ddf65c8b11652
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-22 11:47:42 -0300

    leaving the ItemList component dynamic

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,25 +1,35 @@
 import SingleItem from "./SingleItem"
 
-const ItemList = () => {
+type ItemListProps = {
+    title: string;
+    items: number;
+    itemsArray: Array<{}>
+    path: string;
+}
+
+const ItemList = ({ title, items, itemsArray, path }: ItemListProps) => {
     return (
-        <>
+        <div className="my-3">
 
             <div className="flex justify-between items-center">
-                <h2 className="text-xl font-bold">Artistas populares</h2>
-                <a className="hover:underline text-sm font-bold text-[#a7a7a7]" href="/">
+                <h2 className="text-xl font-bold">{title} populares</h2>
+                <a className="hover:underline text-sm font-bold text-[#a7a7a7]" href={path}>
                     Mostrar tudo
                 </a>
             </div>
 
-            <div className=" w-full flex flex-wrap justify-center md:justify-normal gap-0 mt-5">
-                <SingleItem />
-                <SingleItem />
-                <SingleItem />
-                <SingleItem />
-                <SingleItem />
+            <div className=" w-full flex flex-wrap justify-center items-center gap-0 my-3">
+                {itemsArray.slice(0, items).map((item: any) => (
+                    <SingleItem
+                        {...item}
+                        path={path}
+                        key={`${title}-${item.id}`} 
+                    />
+                ))}
+
 
             </div>
-        </>
+        </div>
     )
 }
 
