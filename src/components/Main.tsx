92cc57ce8f32ce218161daa5e20693aebac7cdbe
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-23 11:10:44 -0300

    modifying Main to be reused in /artists and /songs pages

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,11 +2,27 @@ import ItemList from './ItemList';
 import { artistArray } from './../assets/database/artists.ts';
 import { songsArray } from './../assets/database/songs.ts';
 
-const Main = () => {
+type MainProps = {
+    type?: string;
+}
+
+const Main = ({ type }: MainProps) => {
     return (
         <div className="flex flex-col flex-1 p-5 mt-2 mx-0 rounded-xl overflow-auto bg-[linear-gradient(180deg,rgba(18,18,18,1)_0%,rgba(83,83,83,0)_100%)]">
-            <ItemList title="Artistas" items={7} itemsArray={artistArray} path="/artists"/>
-            <ItemList title="Músicas" items={14}  itemsArray={songsArray} path="/songs"/>
+            {type === 'artists' || !type ? (
+
+                <ItemList title="Artistas" items={7} itemsArray={artistArray} path="/artists" />
+            ) : (
+                <></>
+            )}
+
+            {type === 'songs' || !type ? (
+
+                <ItemList title="Músicas" items={14} itemsArray={songsArray} path="/songs" />
+            ) : (
+                <></>
+            )}
+
         </div>
 
     );
