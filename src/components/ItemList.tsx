5f283044f6d3ca56ae92b00f734b4bcb89a64bc2
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-23 11:10:19 -0300

    modifying ItemList to be reused in /artists and /songs

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import SingleItem from "./SingleItem"
 
 type ItemListProps = {
@@ -9,22 +9,32 @@ type ItemListProps = {
 }
 
 const ItemList = ({ title, items, itemsArray, path }: ItemListProps) => {
+
+    const { pathname } = useLocation();
+
+    const isHome = pathname === '/';
+
+    const finalItems = isHome ? items : Infinity;
+
     return (
         <div className="my-3">
 
             <div className="flex justify-between items-center">
                 <h2 className="text-xl font-bold">{title} populares</h2>
-                <Link className="hover:underline text-sm font-bold text-[#a7a7a7]" to={path}>
-                    Mostrar tudo
-                </Link>
+
+                {isHome && (
+                    <Link className="hover:underline text-sm font-bold text-[#a7a7a7]" to={path}>
+                        Mostrar tudo
+                    </Link>
+                )}
             </div>
 
             <div className=" w-full flex flex-wrap justify-center items-center gap-0 my-3">
-                {itemsArray.slice(0, items).map((item: any) => (
+                {itemsArray.slice(0, finalItems).map((item: any) => (
                     <SingleItem
                         {...item}
                         path={path}
-                        key={`${title}-${item.id}`} 
+                        key={`${title}-${item.id}`}
                     />
                 ))}
 
