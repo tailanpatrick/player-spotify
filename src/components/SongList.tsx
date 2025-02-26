56792dbbf64538c9ed9341d7bd5354f556bf3393
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-26 10:38:56 -0300

    making SongList component dynamic showing an artist's songs actual information

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -1,16 +1,16 @@
+import { Song } from "../types/song"
 import SongItem from "./SongItem"
 
 
-const SongList = () => {
+const SongList = ({ artistSongs }: { artistSongs: Song[] }) => {
+  const items = 5;
+
   return (
     <div className="song-list">
-      <SongItem />
-      <SongItem />
-      <SongItem />
-      <SongItem />
-      <SongItem />
-      <SongItem />
-      <SongItem />
+      {artistSongs.filter((_, index) => index < items )
+      .map(( song, index )=> (
+        <SongItem {...song} key={String(index)} number={index + 1}/>
+      ))}
 
       <p className="ml-6 mt-5 cursor-pointer font-bold hover:underline">Ver mais</p>
     </div>
