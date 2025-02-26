1b7ccddc6e7043c51a599b9554dc9fd06529eaea
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-26 10:35:30 -0300

    making artist page dynamic showing an artist's actual information

--- src/pages/Artist.tsx
+++ src/pages/Artist.tsx
@@ -1,22 +1,40 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import SongList from '../components/SongList';
 
+import { artistArray } from './../assets/database/artists';
+import { ArtistInterface } from '../types/artist';
+import { songsArray } from './../assets/database/songs';
+import { Song } from '../types/song';
+
 const Artist = () => {
+  const { id } = useParams();
+
+  const artist = artistArray.filter(
+    (artist: ArtistInterface) => artist.id === Number(id)
+  )[0] as ArtistInterface;
+
+  const artistSongs = songsArray.filter((song: Song) => song.artist === artist.name) as Song[];
+
+  const randomIndex = Math.floor(Math.random() * (artistSongs.length - 1) );
+  const randomIdSongFromArtist = artistSongs[randomIndex].id;
+
   return (
     <div className="rounded-xl mt-0 mr-0 mb-4">
 
-      <div className="bg-black bg-opacity-25 flex items-end p-6 h-[30svh] md:h-[50svh] bg-cover bg-[40%_40%] bg-[url('https://i.scdn.co/image/ab67618600001016e2952bec9cfd4e6862e23607')]">
-        <h2 className="text-[36px] md:text-8xl md:leading-[100%] text-shadow-md">Jorge & Matheus</h2>
+      <div className={`bg-black bg-opacity-25 flex items-end p-6 h-[30svh] md:h-[50svh] bg-cover bg-[40%_40%]`}
+        style={{backgroundImage: `url(${artist.banner})`}}>
+        <h2 className="text-[36px] md:text-8xl md:leading-[100%] text-shadow-md">{artist.name}</h2>
       </div>
 
       <div className="flex flex-col gap-6 py-6 px-4 md:py-9 md:px-6 bg-[linear-gradient(180deg,rgba(18,18,18,1)_0%,rgba(83,83,83,0)_100%)]">
         <h2 className="text-2xl">Populares</h2>
 
-        <SongList />
+        <SongList artistSongs={artistSongs}/>
       </div>
-      <Link to="/songs/1">
+
+      <Link to={`/songs/${randomIdSongFromArtist}`}>
 
         <FontAwesomeIcon
           icon={faCirclePlay}
