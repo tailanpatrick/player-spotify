56a2dcf1b7be05badc3dbe976f88678dbe78aafc
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-26 09:13:44 -0300

    stylized player component

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,8 +1,44 @@
+import { faBackwardStep, faCirclePlay, faForwardStep } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link } from "react-router-dom"
 
 const Player = () => {
   return (
     <div className="justify-self-stretch flex flex-col items-center gap-1">
-      Player
+      <div className="flex text-2xl items-center gap-5">
+        <Link to="/songs/1">
+
+          <FontAwesomeIcon
+            icon={faBackwardStep}
+            className="cursor-pointer transition-transform duration-200 ease hover:scale-105 hover:text-green-300"
+          />
+        </Link>
+
+        <FontAwesomeIcon
+          icon={faCirclePlay}
+          className="text-[35px] p-3 cursor-pointer transition-transform duration-200 ease hover:scale-105 hover:text-green-300"
+        />
+        <Link to="/songs/3">
+          <FontAwesomeIcon
+            icon={faForwardStep}
+            className="cursor-pointer transition-transform duration-200 ease hover:scale-105 hover:text-green-300"
+          />
+        </Link>
+
+      </div>
+
+      <div className="flex gap-[10px] items-center justify-between w-[100%] max-w-[600px]">
+        <p>00:00</p>
+        <div className="w-[100%] h-1 bg-[#666666] rounded overflow-hidden">
+
+          <div
+            className="h-full bg-white transition-[width] ease duration-200"
+            style={{ width: "var(--_progress)" }}
+          ></div>
+
+        </div>
+        <p>03:40</p>
+      </div>
     </div>
   )
 }
