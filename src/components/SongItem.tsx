f3dfd7038acc48d98c2ced86a0134dfea4dfaa6e
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-26 10:39:58 -0300

    making SongItem component dynamic showing an artist's song actual information

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom"
+import { Song } from "../types/song"
 
-const SongItem = () => {
+
+const SongItem = ({ name, image, duration, id, number } : Song) => {
+
   return (
-    <Link to="/songs/1" className="flex justify-between items-center py-[15px] px-[25px] mr-4 gap-4 rounded hover:bg-[rgba(49,50,50,1)] transition duration-300 ease-in-out cursor-pointer">
+    <Link to={`/songs/${id}`} className="flex justify-between items-center py-[15px] px-[25px] mr-4 gap-4 rounded hover:bg-[rgba(49,50,50,1)] transition duration-300 ease-in-out cursor-pointer">
      <div className="flex items-center gap-6">
-      <p>1</p>
+      <p>{number}</p>
 
       <div className="flex items-center gap-3">
 
-        <img className="w-10 h-10 rounded" src="https://i.scdn.co/image/ab67616d00001e02af41105c5cd91b28f2cf219d" alt="Imagem da Música X" />
-        <p className="hover:underline">Cantada Boba - Ao Vivo</p>
+        <img className="w-10 h-10 rounded" src={`${image}`} alt={`Imagem da música ${name}`} />
+        <p className="hover:underline">{name}</p>
       </div>
 
      </div>
-      <p>03:03</p>
+      <p>{duration}</p>
     </Link>
   )
 }
