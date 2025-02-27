367002b76143756c87ab4803abb28031aa5c8767
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-27 12:03:30 -0300

    refactor to destructing attributes of artist

--- src/pages/Artist.tsx
+++ src/pages/Artist.tsx
@@ -6,16 +6,16 @@ import SongList from '../components/SongList';
 import { artistArray } from './../assets/database/artists';
 import { ArtistInterface } from '../types/artist';
 import { songsArray } from './../assets/database/songs';
-import { Song } from '../types/song';
+import { SongInterface } from '../types/song';
 
 const Artist = () => {
   const { id } = useParams();
 
-  const artist = artistArray.filter(
+  const { name, banner } = artistArray.filter(
     (artist: ArtistInterface) => artist.id === Number(id)
   )[0] as ArtistInterface;
 
-  const artistSongs = songsArray.filter((song: Song) => song.artist === artist.name) as Song[];
+  const artistSongs = songsArray.filter((song: SongInterface) => song.artist === name) as SongInterface[];
 
   const randomIndex = Math.floor(Math.random() * (artistSongs.length - 1) );
   const randomIdSongFromArtist = artistSongs[randomIndex].id;
@@ -24,8 +24,8 @@ const Artist = () => {
     <div className="rounded-xl mt-0 mr-0 mb-4">
 
       <div className={`bg-black bg-opacity-25 flex items-end p-6 h-[30svh] md:h-[50svh] bg-cover bg-[40%_40%]`}
-        style={{backgroundImage: `url(${artist.banner})`}}>
-        <h2 className="text-[36px] md:text-8xl md:leading-[100%] text-shadow-md">{artist.name}</h2>
+        style={{backgroundImage: `url(${banner})`}}>
+        <h2 className="text-[36px] md:text-8xl md:leading-[100%] text-shadow-md">{name}</h2>
       </div>
 
       <div className="flex flex-col gap-6 py-6 px-4 md:py-9 md:px-6 bg-[linear-gradient(180deg,rgba(18,18,18,1)_0%,rgba(83,83,83,0)_100%)]">
