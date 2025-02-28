78b7f43fadb3b07a0ea0633d1cc8a2b0ce6d2bc4
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-28 05:09:21 -0300

    see more functionality added in SongItem component

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -1,18 +1,24 @@
+import { useState } from "react"
 import { SongInterface } from "../types/song"
 import SongItem from "./SongItem"
 
 
 const SongList = ({ artistSongs }: { artistSongs: SongInterface[] }) => {
-  const items = 5;
+  const [items, setItems] = useState(5);
 
   return (
     <div className="song-list">
-      {artistSongs.filter((_, index) => index < items )
-      .map(( song, index )=> (
-        <SongItem {...song} key={String(index)} number={index + 1}/>
-      ))}
+      {artistSongs.filter((_, index) => index < items)
+        .map((song, index) => (
+          <SongItem {...song} key={String(index)} number={index + 1} />
+        ))}
 
-      <p className="ml-6 mt-5 cursor-pointer font-bold hover:underline">Ver mais</p>
+      {items < artistSongs.length && <p className="ml-6 mt-5 cursor-pointer font-bold hover:underline"
+        onClick={() => setItems(prev => prev + 5)}
+      >
+        Ver mais
+      </p>
+      }
     </div>
   )
 }
