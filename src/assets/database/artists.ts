5e6cd0c35150936ca02f99d6c1bf2cca3cfdd74b
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-03-01 13:27:56 -0300

    review artists.TS

--- src/assets/database/artists.ts
+++ src/assets/database/artists.ts
@@ -4,7 +4,7 @@ export const artistArray: Array<ArtistInterface> = [
   {
     id: 1,
     image: "https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4",
-    name: "Henrique & Juliano",
+    name: "Henrique & Juliano ",
     banner: "https://i.scdn.co/image/ab67618600001016b37fbcbd078cb239588df5d9",
   },
   {
