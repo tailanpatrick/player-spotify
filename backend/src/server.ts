d3b6831931e55b96bae84e7d359215bc3b88fce7
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-03-06 12:53:05 -0300

    add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,24 @@
 import express from 'express';
+import cors from 'cors'
 import { artistRoutes } from './routes/artist';
 import { songRoutes } from './routes/song';
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
+
+// Configuração CORS
+const allowedOrigins = [
+  'http://localhost:5173',
+];
+
+app.use(cors())
 
 // Rotas
 app.use('/artist', artistRoutes);
 app.use('/song', songRoutes);
-
-
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
